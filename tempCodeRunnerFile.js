68a88ccb4828c35e38389f54a98d18cb1ec5bf19
creator2104 <vinitr.prajapati2104@gmail.com>
2025-01-20 17:08:41 +0530

    updated file

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+
+// let citites = ['london' , 'new york' , 'paris' , 'berlin']
+// visitedCities = []
+
+// for (let i = 0; i < citites.length; i++) {
+//     if(citites[i]==='paris')
+//     {
+//         continue;
+//     }
+//     visitedCities.push(citites[i])
+// }
+// console.log(visitedCities);
