be334fff55809395adfcffa51ca14b51700fddf0
creator2104 <vinitr.prajapati2104@gmail.com>
2025-02-14 16:11:25 +0530

    made some changes

--- scope.js
+++ scope.js
@@ -10,24 +10,30 @@
 // }
 // one();
 
-if (true) {
-    const username1 = "hitesh"
-    if(username1=="hitesh"){
-        const website = " youtube"
-        console.log(username1+website);
-    }
-    // console.log(website);
-}
-// console.log(username1);
+// if (true) {
+//     const username1 = "hitesh"
+//     if(username1=="hitesh"){
+//         const website = " youtube"
+//         console.log(username1+website);
+//     }
+//     // console.log(website);
+// }
+// // console.log(username1);
 
-console.log(addone(5));
-function addone(num){
-    return num +1
-}
+// console.log(addone(5));
+// function addone(num){
+//     return num +1
+// }
 
-addTwo(5)
-// this is known as hoisting
-// u cant access the value of any variable before initialization 
-const addTwo = function(num){
-    return num+2
-}
+// addTwo(5)
+// // this is known as hoisting
+// // u cant access the value of any variable before initialization 
+// const addTwo = function(num){
+//     return num+2
+// }
+
+let myNumbers = [1,2,3,4,5,6,7,8,9,10]
+let newarr = myNumbers.forEach((i)=> {
+    return i+10
+});
+console.log(newarr);
