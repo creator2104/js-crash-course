770d200868b69a293668a7dd5b2a5fa26dc01754
creator2104 <vinitr.prajapati2104@gmail.com>
2025-03-11 03:52:36 +0530

    changed

--- this2.js
+++ this2.js
@@ -1,12 +1,15 @@
 let obj = {
-    username : "vinit",
-    age : 21,
-    welcomemessage : function (){
-console.log(`${this.username} sir`);
-    }
-}
-obj.welcomemessage()
-obj['username'] = "shail"
-obj.welcomemessage()
+  username: "vinit",
+  age: 21,
+  welcomemessage: function () {
+    console.log(`${this.username} sir`);
+    console.log(this);
+  },
+};
+obj.welcomemessage();
+obj["username"] = "shail";
+obj.welcomemessage();
+
+console.log(this);
 
 // remove this keyword to see the difference
